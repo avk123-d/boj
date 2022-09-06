6c64b42362511d1607a81492d8a14ef56b52698b
정세현 <jeongsehyeon@jeongseung-yeon-ui-MacBookAir.local>
2022-09-06 14:50:29 +0900

    더하기

---
+++ src/boj1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class boj1000 {
+    public static void main(String[] args) {
+
+
+        Scanner one = new Scanner(System.in);
+        int a=one.nextInt();
+        int b=one.nextInt();
+
+        System.out.print(a+b);
+    }
+}
